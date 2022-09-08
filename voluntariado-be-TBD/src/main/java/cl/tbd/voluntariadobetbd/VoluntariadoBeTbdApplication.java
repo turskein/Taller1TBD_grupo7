5c0531ae1adfb6ea48e1c72fed099528dc9eea3f
AlexPacheco <alex.pacheco@usach.cl>
2022-09-08 18:16:57 -0300

    initialize projects

---
+++ voluntariado-be-TBD/src/main/java/cl/tbd/voluntariadobetbd/VoluntariadoBeTbdApplication.java
@@ -0,0 +1,13 @@
+package cl.tbd.voluntariadobetbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntariadoBeTbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoluntariadoBeTbdApplication.class, args);
+    }
+
+}
---
+++ voluntariado-be-TBD/src/test/java/cl/tbd/voluntariadobetbd/VoluntariadoBeTbdApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tbd.voluntariadobetbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntariadoBeTbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

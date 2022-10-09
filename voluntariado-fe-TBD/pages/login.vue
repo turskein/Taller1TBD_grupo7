a93ef101bf5754e0b770845c047e91111afcb830
Francisco Salfate <francisco.salfate.ga@usach.cl>
2022-10-09 15:52:25 -0300

    vista de login

---
+++ voluntariado-fe-TBD/pages/login.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-card
+        class="mx-auto"
+        elevation="2"
+        outlined
+    >
+        <v-card-title class="text-h5">
+            Iniciar sesión
+        </v-card-title>
+        <v-card-subtitle>
+            Ingresar nombre de usuario
+        </v-card-subtitle>
+        <v-card-text>
+            <v-text-field
+                v-model="form.name"
+                outlined
+                :counter="25"
+            />
+        </v-card-text>
+        <v-card-actions>
+            <v-btn
+                color="primary"
+                depressed
+                v-on:click= 'saveName()'
+                href="http://localhost:3000/get_tasks"
+            >
+                Iniciar sesión
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                name: ''
+            }
+        }
+    },
+    methods: {
+        saveName() {
+            localStorage.setItem('name', this.form.name)
+            console.log(localStorage.getItem('name'))
+        }
+    }
+}
+</script>
+
+
